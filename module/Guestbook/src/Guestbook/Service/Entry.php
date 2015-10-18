6b4fbdbac9b4e2d6cf5783bc16f93be75fc24302
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-18 00:14:46 +0000

    add findAll and add function in service

--- module/Guestbook/src/Guestbook/Service/Entry.php
+++ module/Guestbook/src/Guestbook/Service/Entry.php
@@ -1,6 +1,7 @@
 <?php
 namespace Guestbook\Service;
 
+use Guestbook\Entity\Entry as EntryEntity;
 use Zend\EventManager\EventManagerAwareInterface;
 use Zend\EventManager\EventManagerInterface;
 use Zend\ServiceManager\ServiceManagerAwareInterface;
@@ -12,8 +13,40 @@ class Entry implements
 {
     protected $serviceManager;
 
-    protected $eventManager; 
-
+    protected $eventManager;
+    
+    protected $entryMapper;
+    
+    public function add(array $data)
+    {
+        $form = $this->getServiceManager()->get('guestbook_entry_form');
+        $form->bind(new EntryEntity());
+        $form->setData($data);
+    
+        if (!$form->isValid()) {
+            return false;
+        }
+    
+        $entry = $form->getData();
+        $this->getEntryMapper()->add($entry);
+    
+        return $entry;
+    }
+    
+    public function getEntryMapper()
+    {
+        if (null === $this->entryMapper) {
+            $this->entryMapper = $this->getServiceManager()->get('guestbook_entry_mapper');
+        }
+    
+        return $this->entryMapper;
+    }
+    
+    public function findAll()
+    {
+        return $this->getEntryMapper()->findAll();
+    }
+    
     public function getServiceManager()
     {
         return $this->serviceManager;
