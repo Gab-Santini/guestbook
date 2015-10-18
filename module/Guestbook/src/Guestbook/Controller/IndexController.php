a840610a9cf814e5e3623ef7de43301e7e035e7f
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-18 00:16:04 +0000

    use functions service in controller

--- module/Guestbook/src/Guestbook/Controller/IndexController.php
+++ module/Guestbook/src/Guestbook/Controller/IndexController.php
@@ -2,16 +2,30 @@
 namespace Guestbook\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Stdlib\ResponseInterface as Response;
 use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        $prg          = $this->prg();
         $entryForm    = $this->getServiceLocator()->get('guestbook_entry_form');
+        $entryService = $this->getServiceLocator()->get('guestbook_entry_service');
+
+        if ($prg instanceof Response) {
+            return $prg;
+        } elseif ($prg !== false) {
+            $entry = $entryService->add($prg);
+
+            if ($entry) {
+                return $this->redirect()->toRoute('guestbook');
+            }
+        }
+
         return new ViewModel(array(
-                'entryForm' => $entryForm
-                )
-            );
+            'entryForm' => $entryForm,
+            'entries'   => $entryService->findAll()
+        ));
     }
 }
