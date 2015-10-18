4b614e1f59c958d76a68cacc03dd6e13652f9af6
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-18 10:58:04 +0000

    Use post param in controller

--- module/Guestbook/src/Guestbook/Controller/IndexController.php
+++ module/Guestbook/src/Guestbook/Controller/IndexController.php
@@ -2,26 +2,25 @@
 namespace Guestbook\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
-use Zend\Stdlib\ResponseInterface as Response;
 use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        $prg          = $this->prg();
+
         $entryForm    = $this->getServiceLocator()->get('guestbook_entry_form');
         $entryService = $this->getServiceLocator()->get('guestbook_entry_service');
 
-        if ($prg instanceof Response) {
-            return $prg;
-        } elseif ($prg !== false) {
-            $entry = $entryService->add($prg);
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost()->toArray();
+            $entry = $entryService->add($data);
 
             if ($entry) {
                 return $this->redirect()->toRoute('guestbook');
             }
-        }
+          }
 
         return new ViewModel(array(
             'entryForm' => $entryForm,
