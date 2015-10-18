8fe7e9ec9ad354df83e9351a3f9aa2707d20ff80
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-18 00:21:07 +0000

    update service manager to call correctly the entry_mapper

--- module/Guestbook/Module.php
+++ module/Guestbook/Module.php
@@ -1,7 +1,10 @@
 <?php
 namespace Guestbook;
 
-use Zend\Stdlib\Hydrator\ClassMethods;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+
 class Module
 {
     public function getConfig()
@@ -24,16 +27,25 @@ class Module
     {
         return array(
             'invokables' => array(
-                'guestbook_entry_filter' => 'Guestbook\Form\EntryFilter',
-
+                'guestbook_entry_service' => 'Guestbook\Service\Entry',
+				'guestbook_entry_filter' => 'Guestbook\Form\EntryFilter',
+            	'guestbook_entity_entry' => 'Guestbook\Entity\Entry',
             ),
             'factories' => array(
+                'guestbook_entry_mapper' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype($sm->get('guestbook_entity_entry'));
+                    $mapper =  new Entity\EntryTable('guestbook_entry', $dbAdapter, null, $resultSetPrototype);
+                    return $mapper;
+                },    
+                
                 'guestbook_entry_form' => function ($sm) {
                     $form = new Form\Entry();
-                    $form->setHydrator(new ClassMethods());
                     $form->setInputFilter($sm->get('guestbook_entry_filter'));
                     return $form;
                 },
+                
             ),
         );
     }
